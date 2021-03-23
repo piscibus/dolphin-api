0ed6c8f4e55f7df1ada512e9953a99306f793c17
imdhemy <imdhemy@gmail.com>
2021-03-23 20:52:32 +0200

    User can issue a new access token using a refresh token

--- routes/guest.api.php
+++ routes/guest.api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Api\EmailLoginController;
 use App\Http\Controllers\Api\EmailRegistrationController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Passport\Http\Controllers\AccessTokenController;
 
 Route::post('/registration/email', [EmailRegistrationController::class, 'store'])->name('emailRegistration');
 Route::post('/login/email', [EmailLoginController::class, 'store'])->name('emailLogin');
+Route::post('/token/refresh', [AccessTokenController::class, 'issueToken'])->name('refreshToken');
---
+++ tests/Feature/RefreshTokenTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Dolphin\Passport\Repositories\ClientRepository;
+use App\Dolphin\Users\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Throwable;
+
+class RefreshTokenTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     * @throws Throwable
+     */
+    public function test_user_can_issue_access_token_using_a_refresh_token()
+    {
+        $this->withoutExceptionHandling();
+        $this->passportInstall();
+
+        /** @var User $user */
+        $user = User::factory()->create();
+        $loginResponse = $this->postJson(route('emailLogin'), [
+            'email' => $user->getEmail(),
+            'password' => 'password'
+        ])->decodeResponseJson();
+        $token = $loginResponse['data']['token'];
+        $refreshToken = $token['refresh_token'];
+
+        $client = (new ClientRepository())->findPasswordClient();
+        $uri = route('refreshToken');
+        $data = [
+            'grant_type' => 'refresh_token',
+            'refresh_token' => $refreshToken,
+            'client_id' => $client->id,
+            'client_secret' => $client->secret,
+            'scope' => '',
+        ];
+        $response = $this->postJson($uri, $data);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            "token_type",
+            "expires_in",
+            "access_token",
+            "refresh_token"
+        ]);
+    }
+}
