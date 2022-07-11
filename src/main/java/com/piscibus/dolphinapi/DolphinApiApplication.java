ee6afd786dec72e73d2c8f28c0af9362f392d759
Mohamad Eldhemy <imdhemy@gmail.com>
2022-07-11 13:41:04 +0300

    chore(spring): add web package

---
+++ src/main/java/com/piscibus/dolphinapi/DolphinApiApplication.java
@@ -0,0 +1,13 @@
+package com.piscibus.dolphinapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolphinApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DolphinApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/piscibus/dolphinapi/DolphinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piscibus.dolphinapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolphinApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
