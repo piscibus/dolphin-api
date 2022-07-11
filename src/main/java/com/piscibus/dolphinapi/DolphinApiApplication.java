9f4fe3cd78524e4b8f9967fa2d18a63f29d03bd8
Mohamad Eldhemy <imdhemy@gmail.com>
2022-07-11 13:56:09 +0300

    feat(home): add root endpoint

--- src/main/java/com/piscibus/dolphinapi/DolphinApiApplication.java
+++ src/main/java/com/piscibus/dolphinapi/DolphinApiApplication.java
@@ -2,9 +2,25 @@ package com.piscibus.dolphinapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
+@RestController
 public class DolphinApiApplication {
+
+    @RequestMapping("/")
+    public Map<String, String> home() {
+        Map<String, String> res = new HashMap<>();
+
+        res.put("message", "Hello, Dolphin!");
+        res.put("version", "1.0.0");
+
+        return res;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DolphinApiApplication.class, args);
