bb0f031e97dcb792fab55398c47a54b7d225e1af
Mohamad Eldhemy <imdhemy@gmail.com>
2022-07-18 21:24:22 +0300

    feat(user): create role entity

---
+++ src/main/java/com/piscibus/dolphinapi/user/Role.java
@@ -0,0 +1,37 @@
+package com.piscibus.dolphinapi.user;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "roles")
+public class Role {
+    @Id
+    private String id;
+
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Role setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Role setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
