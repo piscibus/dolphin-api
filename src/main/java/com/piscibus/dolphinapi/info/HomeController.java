9580af9ef9932a2bb6ef2db50f4d75f2070b599e
Mohamad Eldhemy <imdhemy@gmail.com>
2022-07-18 20:45:49 +0300

    chore(security): add route placeholders

--- src/main/java/com/piscibus/dolphinapi/info/HomeController.java
+++ src/main/java/com/piscibus/dolphinapi/info/HomeController.java
@@ -7,9 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
     @RequestMapping(path = "/", method = {RequestMethod.GET})
     public ResponseEntity<Dolphin> index() {
         return ResponseEntity.ok(new Dolphin());
+    }
+
+    @RequestMapping(path = "/admin", method = {RequestMethod.GET})
+    public ResponseEntity<String> admin() {
+        return ResponseEntity.ok("<h1>Admin</h1>");
+    }
+
+    @RequestMapping(path = "/user", method = {RequestMethod.GET})
+    public ResponseEntity<String> user() {
+        return ResponseEntity.ok("<h1>User</h1>");
     }
 }
