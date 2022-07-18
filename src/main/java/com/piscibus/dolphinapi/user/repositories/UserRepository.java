0d8ab6dc03b2ea3307fe30a185c7efae8885922e
Mohamad Eldhemy <imdhemy@gmail.com>
2022-07-18 21:28:51 +0300

    chore(user): create user repository

---
+++ src/main/java/com/piscibus/dolphinapi/user/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.piscibus.dolphinapi.user.repositories;
+
+import com.piscibus.dolphinapi.user.entities.User;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
