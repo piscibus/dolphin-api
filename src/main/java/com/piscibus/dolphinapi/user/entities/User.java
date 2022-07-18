ed6a57fe9a6f7ba10596f78de4b63404ec1461be
Mohamad Eldhemy <imdhemy@gmail.com>
2022-07-18 21:28:17 +0300

    chore(user): code refactoring

--- src/main/java/com/piscibus/dolphinapi/user/entities/User.java
+++ src/main/java/com/piscibus/dolphinapi/user/entities/User.java
@@ -1,4 +1,4 @@
-package com.piscibus.dolphinapi.user;
+package com.piscibus.dolphinapi.user.entities;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
