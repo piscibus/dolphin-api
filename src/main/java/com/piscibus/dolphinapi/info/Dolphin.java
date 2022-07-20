d1c0a2b321d202e31e30b96aaacc7cbb11823eda
Mohamad Eldhemy <imdhemy@gmail.com>
2022-07-20 12:11:01 +0300

    feat(props): add applications props

--- src/main/java/com/piscibus/dolphinapi/info/Dolphin.java
+++ src/main/java/com/piscibus/dolphinapi/info/Dolphin.java
@@ -1,15 +1,29 @@
+/**
+ * ----------------------------------------------------------------------------
+ * Dolphin
+ * ----------------------------------------------------------------------------
+ * Dolphin is a customer feedback management tool. This class contains information
+ * about the current version of Dolphin.
+ */
 package com.piscibus.dolphinapi.info;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 
 @Service
+@Configuration("dolphin")
+@ConfigurationProperties(prefix = "piscibus.dolphin")
 public class Dolphin extends HashMap<String, Object> {
+    public final String name = "Dolphin";
+    public final String version = "1.0.0";
+    public final String description = "REST API for Piscibus Dolphin.";
+
     public Dolphin() {
         put("name", "Dolphin");
-        put("species", "Piscibus");
-        put("description", "REST API for Piscibus Dolphin");
         put("version", "1.0.0");
+        put("description", "REST API for Piscibus Dolphin");
     }
 }
